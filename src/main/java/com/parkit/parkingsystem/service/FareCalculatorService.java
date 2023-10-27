131adba3797edf026892bd698bca20fe8b933148
StudicOC <sabrinaleb48@gmail.com>
2023-10-27 12:24:02 +0200

    réduction aux véhicules munis dun ticket discount

--- src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
+++ src/main/java/com/parkit/parkingsystem/service/FareCalculatorService.java
@@ -3,29 +3,58 @@ package com.parkit.parkingsystem.service;
 import com.parkit.parkingsystem.constants.Fare;
 import com.parkit.parkingsystem.model.Ticket;
 
+import java.util.Date;
+
+
+
 public class FareCalculatorService {
 
-    public void calculateFare(Ticket ticket){
-        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ){
-            throw new IllegalArgumentException("Out time provided is incorrect:"+ticket.getOutTime().toString());
+    double discountPercentage = 5;
+
+    public void calculateFare(Ticket ticket, boolean discount){
+        if( (ticket.getOutTime() == null) || (ticket.getOutTime().before(ticket.getInTime())) ) {
+            throw new IllegalArgumentException("Out time provided is incorrect:" + ticket.getOutTime().toString());
         }
 
-        int inHour = ticket.getInTime().getHours();
-        int outHour = ticket.getOutTime().getHours();
 
         //TODO: Some tests are failing here. Need to check if this logic is correct
-        int duration = outHour - inHour;
+        //
+        long outTime = ticket.getOutTime().getTime();
+        long inTime = ticket.getInTime().getTime();
+        float duration =  ((outTime - inTime) / (float)(60*60*1000));
 
+        double timeSpent= (double) Math.round(duration * 1000)/1000;
+
+        // Parking is free for parking times less than 30 minutes ;
+        // A free parking feature for the first 30 minutes;
+        if(timeSpent <= 0.5){
+            ticket.setPrice(0);
+            return;
+        }
+
+        //
         switch (ticket.getParkingSpot().getParkingType()){
             case CAR: {
-                ticket.setPrice(duration * Fare.CAR_RATE_PER_HOUR);
+                ticket.setPrice(timeSpent * Fare.CAR_RATE_PER_HOUR);
                 break;
             }
             case BIKE: {
-                ticket.setPrice(duration * Fare.BIKE_RATE_PER_HOUR);
+                ticket.setPrice(timeSpent * Fare.BIKE_RATE_PER_HOUR);
                 break;
             }
             default: throw new IllegalArgumentException("Unkown Parking Type");
         }
+
+
+        if(discount) {
+            ticket.setPrice( ticket.getPrice() * (1 - (discountPercentage/100)));
+        }
     }
+
+    public void calculateFare(Ticket ticket) {
+     calculateFare(ticket, false);
+    }
+
+
+
 }
